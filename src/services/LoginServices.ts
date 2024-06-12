dc3439599b30b0e0041b6fa1a17b130632ee4915
Leonardo Müller <leonardo.muller@engenharia.ufjf.br>
2024-06-12 03:39:21 -0300

    fixes method login

--- src/services/LoginServices.ts
+++ src/services/LoginServices.ts
@@ -2,7 +2,7 @@ import * as ApiConnector from '@/utils/ApiConector'
 
 export function login($email: string, $password: string) {
   return new Promise((resolve, reject) => {
-    ApiConnector.get('/api/v1/farmacia-tech/auth', {
+    ApiConnector.post('/api/v1/farmacia-tech/auth', {
       email: $email,
       password: $password,
     })
